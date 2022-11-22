df7afef30a0718bbfd5660a5ec34d1c2145bc7f3
Kuldeep Jambhulkar <89466207+kuldeepjambhulkar@users.noreply.github.com>
2022-11-22 08:16:05 +0530

    Added Day-1 program files

---
+++ Day-1/ProgramFiles/Employee.cs
@@ -0,0 +1,22 @@
+﻿ namespace MyProject
+{
+    internal class Employee
+    {
+        int empId;
+        string employeeName;
+        public Employee(int id, string employeeName)
+        {
+            this.empId = id;
+            this.employeeName = employeeName;
+        }
+
+        public int getEmployeeId()
+        {
+            return this.empId;
+        }
+        public string getEmployeeName()
+        {
+            return this.employeeName;
+        }
+    }
+}
---
+++ Day-1/ProgramFiles/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MyProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count;
+        int empId;
+        string empName;
+
+        Console.Write("Enter employee count: ");
+        count = Convert.ToInt32(Console.ReadLine());
+        
+        Employee[] ArrayOfEmployees = new Employee[count];
+        for (var i = 0; i < count; i++) {
+            Console.Write("Enter employee ID: ");
+            empId = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter employee Name: ");
+            empName = Console.ReadLine();
+
+            ArrayOfEmployees[i] = new Employee(empId, empName);
+        }
+
+        Console.WriteLine(ArrayOfEmployees[0].getEmployeeId());
+        Console.WriteLine(ArrayOfEmployees[0].getEmployeeName());
+    }
+}
