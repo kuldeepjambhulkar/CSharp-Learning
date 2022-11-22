bc8544c075f55243cc45f345bcb98810558e644b
Kuldeep Jambhulkar <89466207+kuldeepjambhulkar@users.noreply.github.com>
2022-11-22 09:01:45 +0530

    Added Day-2 program files

---
+++ Day-2/ProgramFiles/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloningAndCopyingObjects
+{
+    internal class Employee
+    {
+        private int empId;
+        private string employeeName;
+
+        public Employee ()
+        {
+
+        }
+        public Employee(int id, string employeeName)
+        {
+            this.empId = id;
+            this.employeeName = employeeName;
+        }
+
+        // copy constructor
+        public Employee( Employee e) {
+            this.empId = e.empId;
+            this.employeeName = e.employeeName;
+        }
+
+        // returns clone of current object
+        public Employee clone() {
+            return new Employee(this.empId, this.employeeName);
+        }
+
+        public void setEmployeeId(int id) { 
+            this.empId = id;
+        }
+
+        public int getEmployeeId()
+        {
+            return this.empId;
+        }
+
+        public void setEmployeeName(string name) { 
+            this.employeeName = name;
+        }
+        public string getEmployeeName()
+        {
+            return this.employeeName;
+        }
+    }
+}
---
+++ Day-2/ProgramFiles/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace CloningAndCopyingObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Employee e1 = new Employee();
+            e1.setEmployeeId(101);
+            e1.setEmployeeName("John");
+            Console.WriteLine("Original Employee e1: " + e1.getEmployeeId() + " " + e1.getEmployeeName());
+            // Output: Original Employee e1: 101 John
+            
+
+
+
+            // Copying e1 into new object e2
+            Employee e2 = e1;
+
+            e2.setEmployeeId(102);
+            e2.setEmployeeName("Alexa");
+            Console.WriteLine("\nCopied Employee e2: " + e2.getEmployeeId() + " " + e1.getEmployeeName());
+            // Output: Copied Employee e2: 102 Alexa
+
+            Console.WriteLine("Original Employee e1: " + e1.getEmployeeId() + " " + e1.getEmployeeName());
+            // Output: Original Employee e1: 102 Alexa
+
+
+
+
+
+
+
+            // Cloning e3 from e1
+            Employee e3 = e1.clone();
+            e3.setEmployeeId(103);
+            e3.setEmployeeName("Smith");
+            Console.WriteLine("\nCloned Employee e3: " + e3.getEmployeeId() + " " + e3.getEmployeeName());
+            // Output: Cloned Employee e3: 103 Smith
+
+            Console.WriteLine("Original Employee e1: " + e1.getEmployeeId() + " " + e1.getEmployeeName());
+            // Output: Original Employee e1: 102 Alexa
+
+
+
+            // Equality comparison
+            bool equality1 = e2 == e1 ? true : false;
+            Console.WriteLine("e2 == e1 => " + equality1);
+            // Output: e2 == e1 => True
+
+            bool equality2 = e3 == e1 ? true : false;
+            Console.WriteLine("e2 == e1 => " + equality2);
+            // Output: e2 == e1 => False
+
+
+
+
+            // Static methods: declared on Type level & not object level
+            // Called with className
+            // More about static methods (https://www.c-sharpcorner.com/UploadFile/abhikumarvatsa/static-methods-in-C-Sharp/)
+            Program.ExampleStaticMethod();
+            // Output: Hey, you are in ExampleStaticMethod()!
+        
+
+            // Instance methods
+            // Methods that can be called by objects, like the setEmployeeID(), getEmployeeName()
+        }
+
+        static void ExampleStaticMethod() {
+            Console.WriteLine("\nHey, you are in ExampleStaticMethod()!");
+        }
+    }
+}
