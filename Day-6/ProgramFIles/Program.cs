642dded214bb0d846571594c17d737b9fc56d06b
Kuldeep Jambhulkar <kuldeepjambhulkar1234@gmail.com>
2022-11-28 20:09:58 +0530

    Added Day-6 files #4

---
+++ Day-6/ProgramFIles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace DelegatesTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            delegateForDoWork del1 = new delegateForDoWork(workPerformed);
+            delegateForWorkDone del2 = new delegateForWorkDone(workDone);
+
+            //del1(4, "Coding");
+            Worker worker = new Worker();
+            worker.doWork(5, "Documenting", del1, del2);
+        }
+
+        public static void workPerformed(int hours, string workType) {
+            Console.WriteLine($"\n{hours} hours completed for {workType}");
+        }
+
+        public static void workDone(string workType) { 
+            Console.WriteLine($"{workType} completed");
+        }
+
+    }
+}
---
+++ Day-6/ProgramFIles/Worker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DelegatesTutorial
+{
+    public delegate void delegateForDoWork(int hours, string workType);
+    public delegate void delegateForWorkDone(string workType);
+
+
+    internal class Worker
+    {
+        public void doWork(int hours, string workType, delegateForDoWork del1, delegateForWorkDone del2) {
+            for (int i = 0; i <= hours; i++) {
+                del1(i + 1, workType);
+            }
+            del2(workType);
+        }
+    }
+}
