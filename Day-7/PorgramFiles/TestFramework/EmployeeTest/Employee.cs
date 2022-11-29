e08692182479faf1b1cc49be593957b3e2f9b5ce
Kuldeep Jambhulkar <89466207+kuldeepjambhulkar@users.noreply.github.com>
2022-11-29 08:55:30 +0530

    Added Day-7 #5

---
+++ Day-7/PorgramFiles/TestFramework/EmployeeTest/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeTest
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public void printSomething(int a) { 
+            Console.WriteLine(a);
+        }
+
+        public void printSomething(int a, char b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void printSomething(string a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public virtual void OverrideThisMethod() {
+            Console.WriteLine("This is overriden method");
+        }
+    }
+}
