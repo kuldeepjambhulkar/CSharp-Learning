1b6194bf362f2c9da5f3cb3b971969f981d25dfb
Kuldeep Jambhulkar <89466207+kuldeepjambhulkar@users.noreply.github.com>
2022-12-13 21:00:13 +0530

    Add Day-8 #6

---
+++ Day-8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program2.method3();
+
+            int a = 23, b = 0;
+            try
+            {
+                if (a == b)
+                {
+                    throw new Exception("\na & b shouldn't be equal!");
+                }
+                else
+                {
+                    try
+                    {
+                        if (b == 0)
+                        {
+                            throw new Exception("\n b shouldn't be zero");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                }
+
+            }
+            catch (Exception e) { 
+                Console.WriteLine(e.ToString());
+                try {
+                    if (true) {
+                        throw new Exception("\n\nExpression shouldn't be true!");
+                    }
+                } catch (Exception e1) {
+                    Console.WriteLine(e1.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Day-8/Program2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class Program2
+    {
+        public static void method3() {
+            try
+            {
+                method2();
+            }
+            catch (Exception ex) { 
+                Console.WriteLine(ex.StackTrace.ToString());
+            }
+        }
+        public static void method2() {
+            try
+            {
+                method1();
+            }
+            catch (Exception ex) {
+                throw;
+            }
+        }
+        public static void method1() {
+            try
+            {
+                throw new Exception("Exception from method1()");
+            }
+            catch (Exception ex) {
+                throw ex;
+            }
+        }
+
+    }
+}
